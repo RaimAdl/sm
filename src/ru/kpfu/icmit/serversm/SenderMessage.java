77df8b5f71ebd66550a5400904e3b239b2ac5aad
RaimAdl <raim100199@gmail.com>
2017-12-14 22:20:48 +0300

    Update SenderMessage.java

--- src/ru/kpfu/icmit/serversm/SenderMessage.java
+++ src/ru/kpfu/icmit/serversm/SenderMessage.java
@@ -1,15 +1,34 @@
 package ru.kpfu.icmit.serversm;
 
 public class SenderMessage {
-    public SenderMessage(String message){
+  public static boolean sendMsg(String message){
+     Gson cm = new Gson();
+     Message n = cm.fromJson(message, Message.class);
+     Statement st = null;
+     try {
+         st = DbWork.initDb().createStatement();
+         ResultSet rs = st.executeQuery(
+                 "select id from users where sean_id='"+n.token+"'");
+         String user_id="0";
+         if (rs.next()) {
+             user_id = rs.getString("id");
+         }
+         st = DbWork.initDb().createStatement();
+         st.execute("insert into messages (fromuser, touser,content,datamsg) values (" +user_id+","+n.abonent+",'"+
+                 n.content+"','"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"')");
+         return true;
+     } catch (SQLException e) {
+         e.printStackTrace();
+     }
 
-    }
+ // 1. Проверка токена пользователя
 
-    public String sendMsg(){
-        // 1. Проверка токена пользователя
+ return false;
+ }
+ class Message{
+     String token;
+     String abonent;
+     String content;
 
-        return null;
-    }
-
-
+ }
 }
