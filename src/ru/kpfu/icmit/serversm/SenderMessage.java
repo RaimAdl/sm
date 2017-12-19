95eebf5100f2b0d53fa5edc707dd7a45b000f01c
RaimAdl <raim100199@gmail.com>
2017-12-19 13:46:04 +0300

    Update SenderMessage.java

--- src/ru/kpfu/icmit/serversm/SenderMessage.java
+++ src/ru/kpfu/icmit/serversm/SenderMessage.java
@@ -1,15 +1,46 @@
-package ru.kpfu.icmit.serversm;
+ package ru.kpfu.icmit.serversm;
 
-public class SenderMessage {
-    public SenderMessage(String message){
+ import com.google.gson.Gson;
 
-    }
+ import java.sql.ResultSet;
+ import java.sql.SQLException;
+ import java.sql.Statement;
+ import java.text.SimpleDateFormat;
+ import java.util.Date;
 
-    public String sendMsg(){
-        // 1. Проверка токена пользователя
+ /**
+  * @autor Adilgereyev
+  */
+ public class SenderMessage {
 
-        return null;
-    }
+ public static boolean sendMsg(String message){
+     Gson cm = new Gson();
+     Message n = cm.fromJson(message, Message.class);
+     Statement st = null;
+     try {
+         st = DbWork.initDb().createStatement();
+         ResultSet rs = st.executeQuery(
+                 "select id from users where sean_id='"+n.token+"'");
+         String user_id="0";
+         if (rs.next()) {
+             user_id = rs.getString("id");
+         }
+         st = DbWork.initDb().createStatement();
+         st.execute("insert into messages (fromuser, touser,content,datamsg) values (" +user_id+","+n.abonent+",'"+
+                 n.content+"','"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"')");
+         return true;
+     } catch (SQLException e) {
+         e.printStackTrace();
+     }
 
+ // 1. Проверка токена пользователя
 
-}
+ return false;
+ }
+ class Message{
+     String token;
+     String abonent;
+     String content;
+
+ }
+ }
